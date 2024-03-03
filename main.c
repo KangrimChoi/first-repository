b59e1dd862a3f54e60d49acf4913aaf1afd84e27
KangrimChoi <160142922+KangrimChoi@users.noreply.github.com>
2024-03-03 14:13:34 +0000

    third commit

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 int main(){
   printf("Hello world\n");
   printf("Hello wrold2\n");
+  printf("Hello wrold3\n");
   
   return 0;
 }
+
+// git 유용한 명령어
+/*
+git log : commit 로그 볼 수 있음.
+git commit -am : 스테이징 + 저장소로 파일 보내기 한 번에 수행.
+*/
